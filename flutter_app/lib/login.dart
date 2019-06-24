4f4f8d76310aa809e68ecb1fcdcf0665c50856d8
Homero J. Simpson <said.g@hotmail.com>
2019-06-24 09:11:39 -0500

    first commit

---
+++ flutter_app/lib/login.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  Widget boton(String text) {
+    return Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: Center(
+          child: Text(text,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 20)),
+        ),
+        height: 70.0,
+        width: 300.0,
+        decoration: new BoxDecoration(
+          color: Colors.black,
+          borderRadius: new BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+          /*
+                      * boxShadow: <BoxShadow> [
+                        new BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10.0,
+                          offset: new Offset(0.0, 10.0)
+                        )
+                      ]*/
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: new Container(
+        color: Colors.white,
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            boton("Sign In"),
+            boton("Log In"),
+          ],
+        )),
+      ),
+    );
+  }
+}
