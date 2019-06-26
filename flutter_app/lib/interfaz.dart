48dcd7225444ee88ae99a43ae5be8da87fd22624
Homero J. Simpson <said.g@hotmail.com>
2019-06-26 10:06:50 -0500

    Añadiendo genero a las peliculas

--- flutter_app/lib/interfaz.dart
+++ flutter_app/lib/interfaz.dart
@@ -2,8 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Interfaz extends StatelessWidget {
-
-  Widget titulo(){
+  Widget titulo() {
     return Container(
       margin: EdgeInsets.only(left: 22.0, top: 25.0, bottom: 10.0),
       child: Text(
@@ -18,25 +17,49 @@ class Interfaz extends StatelessWidget {
   }
 
   Widget firstCard() {
-    return Container(
-      margin: EdgeInsets.only(left: 23.0, right: 15.0),
-      height: 355.0,
-      width: 200.0,
-      decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 23.0, right: 15.0),
+          height: 355.0,
+          width: 200.0,
+          decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 23.0, right: 15.0, top: 10.0),
+          width: 200.0,
+          child: Text(
+            "Genero",
+            textAlign: TextAlign.left,
+            style: TextStyle(color: Colors.black26),
+          ),
+        )
+      ],
     );
   }
 
   Widget card() {
-    return Container(
-      margin: EdgeInsets.only(right: 15.0),
-      height: 355.0,
-      width: 200.0,
-      decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.all(Radius.circular(10.0))),
-    );
+    return Column(children: <Widget>[
+      Container(
+        margin: EdgeInsets.only(right: 15.0),
+        height: 355.0,
+        width: 200.0,
+        decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      ),
+      Container(
+        margin: EdgeInsets.only(right: 15.0, top: 10.0),
+        width: 200.0,
+        child: Text(
+          "Genero",
+          textAlign: TextAlign.left,
+          style: TextStyle(color: Colors.black26),
+        ),
+      )
+    ]);
   }
 
   Widget tabBar() {
@@ -56,7 +79,8 @@ class Interfaz extends StatelessWidget {
 
   Widget listHorizontal() {
     return Container(
-        height: 370.0,
+        height: 390.0,
+        // TODO Convertir esta ListVIew() en un ListView.builder
         child: ListView(scrollDirection: Axis.horizontal, children: <Widget>[
           firstCard(),
           card(),
@@ -71,24 +95,20 @@ class Interfaz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height * 0.93,
-            child: ListView(
-              scrollDirection: Axis.vertical,
-              children: <Widget>[
-                titulo(),
-                listHorizontal()
-              ],
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.93,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: <Widget>[titulo(), listHorizontal()],
+              ),
             ),
-          ),
-          tabBar()
-        ],
-      )
-    );
+            tabBar()
+          ],
+        ));
   }
 }
